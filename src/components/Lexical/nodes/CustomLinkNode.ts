29d83c63ac3fa1dd2e1f506ad7030bcded28dd54
Sissi <sylvia.adamski@gmail.com>
2024-08-19 15:58:43 +0200

    fix lexical warnings regarding importJSON (only some)

--- src/components/Lexical/nodes/CustomLinkNode.ts
+++ src/components/Lexical/nodes/CustomLinkNode.ts
@@ -1,12 +1,21 @@
 import { LinkNode } from '@lexical/link';
 
 export class CustomLinkNode extends LinkNode {
+
   static getType() {
     return "custom-link";
   }
 
   static clone(node: any) {
     return new CustomLinkNode(node.getURL(), { rel: node.getRel(), target: node.getTarget() }, node.getKey());
+  }
+
+  exportJSON() {
+    return {
+      ...super.exportJSON(),
+      type: 'custom-link',
+      version: 1,
+    };
   }
 
   createDOM(config: any) {
